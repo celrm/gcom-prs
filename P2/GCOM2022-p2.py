58e0419bf64308627ede9e227c2031366acc5c0f
Celia Rubio <celia_rubio@hotmail.es>
2022-02-23 01:00:35 +0100

    memoria p2

--- P2/GCOM2022-p2.py
+++ P2/GCOM2022-p2.py
@@ -1,27 +1,26 @@
-"""
+'''
 Práctica 2
-"""
+'''
 import numpy as np
 import pandas as pd
 from collections import Counter
 
-# Esto me permite realizar el apartado iii) en las mismas condiciones que la plantilla.
-# Esta columna extra sirve para desambiguar el orden de los elementos de igual probabilidad.
-sorting_list = ["T","p","C","m","A","k","q",";","b","f","y","?","W","B","v","’","\n"
-,"-","S",",","l","r",".","d","n","I","c","w","g","i","u","a","o","s","e","t","h"," ",] # iii)
-sorting_dict = {l: i for i, l in enumerate(sorting_list)}  # iii)
+# Me permite hacer el apartado iii) en las mismas condiciones que la plantilla.
+# Columna extra para desambiguar el orden de los elems de igual probabilidad.
+col = 'TpCmAkq;bfy?WBv’\n-S,lr.dnIcwgiuaoseth '
+col = {l: i for i, l in enumerate(col)}
 
 # Dado el nombre de un fichero, devuelve un Dataframe con columnas
-# "state" (letras) y "probabs" (frecuencias normalizadas) ordenado
-# de menor a mayor frecuencia. A =, se desambigua por sorting_dict.
+# 'state' (letras) y 'probabs' (frecuencias normalizadas) ordenado
+# de menor a mayor frecuencia. Se desambigua por col.
 def distribution_from_file(path):
-    contents = open(path, "r", encoding="utf8").read()
+    contents = open(path, 'r', encoding='utf8').read()
     frecs = Counter(contents)  # cuento la frecuencia de cada letra
-    distr = pd.DataFrame(frecs.most_common(), columns=["state", "probab"])
+    distr = pd.DataFrame(frecs.most_common(), columns=['state', 'probab'])
     distr.probab = distr.probab / float(sum(distr.probab))  # normalizo probabs
-    distr["sort"] = [sorting_dict.get(l, 0) for l in distr.state] # iii)
-    distr.sort_values(["probab", "sort"], ascending=True, ignore_index=True, inplace=True)
-    del distr["sort"] # iii)
+    distr['s'] = [col.get(l, 0) for l in distr.state] # iii)
+    distr.sort_values(['probab', 's'], ascending=True, ignore_index=True, inplace=True)
+    del distr['s'] # iii)
     return distr
 
 
@@ -29,18 +28,18 @@ def distribution_from_file(path):
 # introduce el nuevo elemento, y reordena de nuevo.
 def huffman_branch(distr):
     code = {
-        "value0": distr.state[0],
-        "value1": distr.state[1],
-        "child0": distr.child[0],  # para recorrer el árbol luego
-        "child1": distr.child[1],  # para recorrer el árbol luego
+        'value0': distr.state[0],
+        'value1': distr.state[1],
+        'child0': distr.child[0],  # para recorrer el árbol luego
+        'child1': distr.child[1],  # para recorrer el árbol luego
     }
     distr.loc[len(distr.index)] = {
-        "state": distr.state[0] + distr.state[1],  # concat states
-        "probab": distr.probab[0] + distr.probab[1],  # sum probabs
-        "child": len(distr.index) - 2,  # para recorrer el árbol luego
+        'state': distr.state[0] + distr.state[1],  # concat states
+        'probab': distr.probab[0] + distr.probab[1],  # sum probabs
+        'child': len(distr.index) - 2,  # para recorrer el árbol luego
     }
     distr = distr.drop([0, 1])
-    distr.sort_values("probab", ascending=True, ignore_index=True, inplace=True)
+    distr.sort_values('probab', ascending=True, ignore_index=True, inplace=True)
     return distr, code
 
 
@@ -48,7 +47,7 @@ def huffman_branch(distr):
 # Una lista que empieza en el 0 y se recorre con cada índice childx.
 def huffman_tree(distr):
     distribution = distr.copy()
-    distribution["child"] = None  # si son hojas se quedarán así
+    distribution['child'] = None  # si son hojas se quedarán así
     tree = []
     while len(distribution) > 1:
         distribution, code = huffman_branch(distribution)
@@ -60,32 +59,32 @@ def huffman_tree(distr):
 def codif_table(tree):
     codification = {}
     for code in tree:
-        for l in code["value0"]:
-            codification[l] = "0" + codification.get(l, "")
-        for l in code["value1"]:
-            codification[l] = "1" + codification.get(l, "")
+        for l in code['value0']:
+            codification[l] = codification.get(l, '') + '0'
+        for l in code['value1']:
+            codification[l] = codification.get(l, '') + '1'
     return codification
 
 
 # Para el apartado ii)
 # Dado un diccionario letra->código, devuelve la codificación de una palabra.
 def codification(codif, word):
-    return "".join([codif[l] for l in word])
+    return ''.join([codif[l] for l in word])
 
 
 # Para el apartado iii)
-# Dado un árbol de Huffman y una palabra codificada, devuelve su palabra original.
+# Dado un árbol de Huffman y una palabra codificada, devuelve su original.
 # El árbol es una lista cuya raíz es el primer elemento.
-# Cada elemento tiene Valor (0),  Valor (1), Índice del hijo (0) e Índice del hijo (1).
+# Cada elem: Valor (0),  Valor (1), Índice del hijo (0) e Índice del hijo (1).
 def decodification(tree, code):
-    word = ""
+    word = ''
     i = 0
     for c in code:
-        if not tree[i]["child" + c]:
-            word += tree[i]["value" + c]
+        if not tree[i]['child' + c]:
+            word += tree[i]['value' + c]
             i = 0
         else:
-            i = tree[i]["child" + c]
+            i = tree[i]['child' + c]
     return word
 
 
@@ -99,30 +98,30 @@ def get_H(distr):
     return -sum(distr.probab * np.log2(distr.probab))
 
 
-""" Apartado i) """
-print("-" * 10)
+''' Apartado i) '''
+print('-' * 10)
 
-distr_eng = distribution_from_file("GCOM2022_pract2_auxiliar_eng.txt")
+distr_eng = distribution_from_file('GCOM2022_pract2_auxiliar_eng.txt')
 tree_eng = huffman_tree(distr_eng)
 codif_eng = codif_table(tree_eng)
 L_eng = get_L(distr_eng, codif_eng)
 H_eng = get_H(distr_eng)
 
-print("Inglés:\t\tH =", H_eng, "≤ L =", L_eng, "< H+1 =", H_eng + 1)
+print('Inglés:\t\tH =', H_eng, '≤ L =', L_eng, '< H+1 =', H_eng + 1)
 
-distr_esp = distribution_from_file("GCOM2022_pract2_auxiliar_esp.txt")
+distr_esp = distribution_from_file('GCOM2022_pract2_auxiliar_esp.txt')
 tree_esp = huffman_tree(distr_esp)
 codif_esp = codif_table(tree_esp)
 L_esp = get_L(distr_esp, codif_esp)
 H_esp = get_H(distr_esp)
 
-print("Español:\tH =", H_esp, "≤ L =", L_esp, "< H+1 =", H_esp + 1)
+print('Español:\tH =', H_esp, '≤ L =', L_esp, '< H+1 =', H_esp + 1)
 
 
-""" Apartado ii) """
-print("-" * 10)
+''' Apartado ii) '''
+print('-' * 10)
 
-word = "medieval"
+word = 'medieval'
 
 # Creamos la tabla de codificación binaria para el conjunto de todas las letras de los textos.
 # La tabla no hace falta para saber la longitud de la palabra codificada, pues será
@@ -136,15 +135,17 @@ result_bin = codification(codif_bin, word)
 result_eng = codification(codif_eng, word)
 result_esp = codification(codif_esp, word)
 
-print("Codificación de la palabra:", word)
-print("En binario (hay", len(all_letters), "letras):\t", len(word) * size_bin, result_bin)
-print("Huffman inglés:\t\t\t", len(result_eng), result_eng)
-print("Huffman español:\t\t", len(result_esp), result_esp)
+print('Codificación de la palabra:', word)
+print('En binario (hay', len(all_letters), 'letras):', len(word), '*', size_bin, '=', len(word) * size_bin, result_bin)
+print('Huffman inglés:\t\t\t', len(result_eng), result_eng)
+print('Huffman español:\t\t', len(result_esp), result_esp)
 
-""" Apartado iii) """
-print("-" * 10)
+print("Comprobación:",decodification(tree_eng,result_eng),decodification(tree_esp,result_esp))
 
-code = "10111101101110110111011111"
+''' Apartado iii) '''
+print('-' * 10)
+
+code = '10111101101110110111011111'
 word = decodification(tree_eng, code)
-print("Decodificación del código:", code)
+print('Decodificación del código:', code)
 print(word)
