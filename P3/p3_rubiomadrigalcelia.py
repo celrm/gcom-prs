9526e8f560692a097de7bccd14897b534084fd4a
Celia Rubio <celia_rubio@hotmail.es>
2022-03-02 15:09:57 +0100

    plots, imgs and labels

--- P3/p3_rubiomadrigalcelia.py
+++ P3/p3_rubiomadrigalcelia.py
@@ -10,6 +10,7 @@ from scipy.spatial import Voronoi, voronoi_plot_2d
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 
+Figure = 1
 
 """ Apartado i) """
 
@@ -23,19 +24,24 @@ X, labels_true = make_blobs(
 
 # #############################################################################
 # Los clasificamos mediante el algoritmo KMeans
-def plot_silhouette(xs, alg):
+def plot_silhouette(param, xs, alg):
+    global Figure
     ss = []
     for x in xs:
         m = alg(x).fit(X)
         silhouette = metrics.silhouette_score(X, m.labels_)
         k = len(set(m.labels_)) - (1 if -1 in m.labels_ else 0)
-        print("Param. = %0.3f" % x, "\tk =", k, "\ts̄ = %0.3f" % silhouette)
+        print(param,"= %0.3f" % x, "\tk =", k, "\ts̄ = %0.3f" % silhouette)
         ss += [silhouette]
+    plt.xlabel(param)
+    plt.ylabel("Silhouette Coefficient s̄")
     plt.plot(xs, ss)
+    plt.savefig("Figure_%d" % Figure)
+    Figure += 1
     plt.show()
 
 
-plot_silhouette(range(2, 4), lambda x: KMeans(n_clusters=x, random_state=0))
+plot_silhouette("k",range(2, 16), lambda x: KMeans(n_clusters=x, random_state=0))
 kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 print("\nCentros:\n", kmeans.cluster_centers_)
 
@@ -82,9 +88,8 @@ def plot_points(alg, core_samples_mask=None):
             markeredgecolor="k",
             markersize=3,
         )
-
-    n_clusters = len(set(alg.labels_)) - (1 if -1 in alg.labels_ else 0)
-    plt.title("Number of clusters: %d" % n_clusters)
+    plt.xlabel("x")
+    plt.ylabel("y")
 
 
 vor_regions = Voronoi(kmeans.cluster_centers_)
@@ -94,6 +99,9 @@ plot_points(kmeans)
 plt.plot(
     kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], "ro", markersize=8
 )
+plt.title("KMEANS algorithm\nNumber of clusters: %d" % len(kmeans.cluster_centers_))
+plt.savefig("Figure_%d" % Figure)
+Figure += 1
 plt.show()
 
 
@@ -102,7 +110,7 @@ plt.show()
 # #############################################################################
 # Los clasificamos mediante el algoritmo DBSCAN
 
-plot_silhouette(
+plot_silhouette("eps",
     np.arange(0.1, 0.4, 0.02),
     lambda x: DBSCAN(eps=x, min_samples=10, metric="euclidean"),
 )
@@ -110,11 +118,16 @@ db = DBSCAN(eps=0.28, min_samples=10, metric="euclidean").fit(X)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 plot_points(db, core_samples_mask)
+
+n_clusters_ = len(set(db.labels_)) - (1 if -1 in db.labels_ else 0)
+plt.title("DBSCAN+euclidean algorithm\nNumber of clusters: %d" % n_clusters_)
+plt.savefig("Figure_%d" % Figure)
+Figure += 1
 plt.show()
 
 print("-" * 10)
 
-plot_silhouette(
+plot_silhouette("eps",
     np.arange(0.1, 0.4, 0.02),
     lambda x: DBSCAN(eps=x, min_samples=10, metric="manhattan"),
 )
@@ -122,6 +135,11 @@ db = DBSCAN(eps=0.36, min_samples=10, metric="manhattan").fit(X)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 plot_points(db, core_samples_mask)
+
+n_clusters_ = len(set(db.labels_)) - (1 if -1 in db.labels_ else 0)
+plt.title("DBSCAN+manhattan algorithm\nNumber of clusters: %d" % n_clusters_)
+plt.savefig("Figure_%d" % Figure)
+Figure += 1
 plt.show()
 
 print("-" * 10)
@@ -140,4 +158,8 @@ voronoi_plot_2d(vor_regions, show_points=False, show_vertices=False)
 
 plot_points(kmeans)
 plt.plot(problem[:, 0], problem[:, 1], "ro", markersize=8)
+
+plt.title("Class Predictions")
+plt.savefig("Figure_%d" % Figure)
+Figure += 1
 plt.show()
